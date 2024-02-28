e8733ceac47afe43d182b65228619a86b620a95f
Bot Hunting <82310684+BotHunting@users.noreply.github.com>
2024-02-28 21:28:18 +0900

    navbar.php
    Signed-off-by: Bot Hunting <82310684+BotHunting@users.noreply.github.com>

--- navbar.php
+++ navbar.php
@@ -77,3 +77,6 @@ if(isset($_SESSION['username'])){
         </div>
     </div>
 </nav>
+</body>
+<script type='text/javascript' src='https://assets.trakteer.id/js/trbtn-overlay.min.js'></script><script type='text/javascript' class='troverlay'>(function() {var trbtnId = trbtnOverlay.init('Support Me','#FFC147','https://trakteer.id/hunty/tip/embed/modal','https://cdn.trakteer.id/images/embed/trbtn-icon.png?date=18-11-2023','35','floating-right');trbtnOverlay.draw(trbtnId);})();</script>
+</html>
